7e248826d2ce7c72961ca088088090b03c00c825
luxiao <1478488313@qq.com>
2024-03-01 08:06:49 +0800

    struct-v1.1

--- struct/struct.go
+++ struct/struct.go
@@ -4,3 +4,8 @@ func Perimeter(width float64, height float64) (perimeter float64) {
 	perimeter = (width + height) * 2
 	return
 }
+
+func Area(width float64, height float64) (Area float64) {
+	Area = width * height
+	return
+}
