f9cbc646d14a8a370c346e2ce87619898060aaad
luxiao <1478488313@qq.com>
2024-03-03 21:06:22 +0800

    map-v1.3

--- map/map_test.go
+++ map/map_test.go
@@ -19,6 +19,16 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+
+	dictionary.Add(word, definition)
+
+	assertDefinition(t, dictionary, word, definition)
+}
+
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 
@@ -32,5 +42,18 @@ func assertError(t *testing.T, got, want error) {
 
 	if got != want {
 		t.Errorf("got error '%s' want '%s'", got, want)
+	}
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if definition != got {
+		t.Errorf("got '%s' want '%s'", got, definition)
 	}
 }
