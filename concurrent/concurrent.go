4234396aa659560e209e3baeb3e18e9491594e4c
luxiao <1478488313@qq.com>
2024-03-04 14:24:31 +0800

    concurrent-v1.1

--- concurrent/concurrent.go
+++ concurrent/concurrent.go
@@ -2,12 +2,22 @@ package main
 
 type WebsiteChecker func(string) bool
 
+type result struct {
+	string
+	bool
+}
+
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
-
+	resultChannel := make(chan result)
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(url string) {
+			resultChannel <- result{url, wc(url)}
+		}(url)
 	}
-
+	for i := 0; i < len(urls); i++ {
+		val := <-resultChannel
+		results[val.string] = val.bool
+	}
 	return results
 }
