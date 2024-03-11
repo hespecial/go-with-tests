66d513c5372d8b69f183e582c54384e2fa9138df
luxiao <1478488313@qq.com>
2024-03-11 12:00:04 +0800

    Add some comments

--- 13_sync/v1/sync.go
+++ 13_sync/v1/sync.go
@@ -1,13 +1,16 @@
 package main
 
+// Counter will increment a number.
 type Counter struct {
 	value int
 }
 
+// Inc the count.
 func (c *Counter) Inc() {
 	c.value++
 }
 
+// Value returns the current count.
 func (c *Counter) Value() int {
 	return c.value
 }
--- 13_sync/v2/sync.go
+++ 13_sync/v2/sync.go
@@ -2,21 +2,25 @@ package main
 
 import "sync"
 
+// Counter will increment a number.
 type Counter struct {
 	mu    sync.Mutex
 	value int
 }
 
+// NewCounter returns a new Counter.
 func NewCounter() *Counter {
 	return &Counter{}
 }
 
+// Inc the count.
 func (c *Counter) Inc() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	c.value++
 }
 
+// Value returns the current count.
 func (c *Counter) Value() int {
 	return c.value
 }
